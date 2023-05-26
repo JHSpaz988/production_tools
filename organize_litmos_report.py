ed09f02a04cecacb6010e5ce1da1f15f7ef73c67
Jaron Haberstroh <JHSpaz988@gmail.com>
2023-05-26 03:51:29 -0400

    Adding in argv so it works from the command line

--- organize_litmos_report.py
+++ organize_litmos_report.py
@@ -1,30 +1,42 @@
 import pandas
+import sys
+import os
 
-with open("/home/jhspaz988/Downloads/Summary - People Report.csv") as file:
-    data = pandas.read_csv(file)
 
-data_keys = list(data.keys())
+def organize_litmos_report(ofp: str, wfp: str) -> None:
+    with open(ofp) as file:
+        data = pandas.read_csv(file)
 
-for index in range(1, 3):
-    data.drop(data_keys[1], inplace=True, axis=1)
-    data_keys.remove(data_keys[1])
+    data_keys = list(data.keys())
 
-data = data.sort_values(by=data_keys[-1], ascending=True)
+    for index in range(1, 3):
+        data.drop(data_keys[1], inplace=True, axis=1)
+        data_keys.remove(data_keys[1])
 
-sheets = []
-shift_keys = data[data_keys[-1]].unique().tolist()
-for sheet in shift_keys:
-    new_data = data[(data[data_keys[-1]] == sheet) & (data[data_keys[1]] < 100)]
-    sheet = pandas.DataFrame(new_data)
-    sheets.append(new_data)
+    data = data.sort_values(by=data_keys[-1], ascending=True)
 
-blank_sheet = data[(data[data_keys[-1]].isna()) & (data[data_keys[1]] < 100)]
-blank_sheet = pandas.DataFrame(blank_sheet)
+    sheets = []
+    shift_keys = data[data_keys[-1]].unique().tolist()
+    for sheet in shift_keys:
+        new_data = data[(data[data_keys[-1]] == sheet)
+                        & (data[data_keys[1]] < 100)]
+        sheet = pandas.DataFrame(new_data)
+        sheets.append(new_data)
 
-with pandas.ExcelWriter("/home/jhspaz988/Documents/shift_sorted.xlsx", "openpyxl") as writer:
-    for (index, sheet) in enumerate(sheets):
-        if sheet.empty:
-            pass
-        else:
-            sheet.to_excel(writer, sheet_name=shift_keys[index], index=False)
-    blank_sheet.to_excel(writer, sheet_name="None", index=False)
+    blank_sheet = data[(data[data_keys[-1]].isna())
+                       & (data[data_keys[1]] < 100)]
+    blank_sheet = pandas.DataFrame(blank_sheet)
+
+    with pandas.ExcelWriter(wfp, "openpyxl") as writer:
+        for (index, sheet) in enumerate(sheets):
+            if sheet.empty:
+                pass
+            else:
+                sheet.to_excel(
+                    writer, sheet_name=shift_keys[index], index=False)
+        blank_sheet.to_excel(writer, sheet_name="None", index=False)
+
+
+open_file_path = os.path.abspath(sys.argv[1])
+write_file_path = os.path.abspath(sys.argv[2])
+organize_litmos_report(ofp=open_file_path, wfp=write_file_path)
